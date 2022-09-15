eba2e57945c9d99ab7147f843e01d23ff8fab1ca
cjfinan <cjfinan98@gmail.com>
2022-09-15 10:52:38 +0000

    retrieve latest stock numbers from speadsheet

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gspread
 from google.oauth2.service_account import Credentials
+from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -11,6 +12,7 @@ CREDS = Credentials.from_service_account_file("creds.json")
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+
 
 def get_sales_data():
     """
@@ -31,7 +33,7 @@ def get_sales_data():
 
     return sales_data
 
-    
+
 def validate_data(values):
     """
     Inside the try, converts all string values to integers, Raises ValueError
@@ -46,9 +48,10 @@ def validate_data(values):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
     return True
-        
+
+
 def update_sales_worksheet(data):
     """
     Update sales worksheet, add new row with the list data provided
@@ -58,6 +61,30 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("sales worksheet updated correctly \n")
 
-data = get_sales_data()
-sales_data = [int(num) for num in data]
-update_sales_worksheet(sales_data)
+
+def calculate_surplus_data(sales_row):
+    """
+    Comapare sales with stock and calculate the surplus for each item type.
+
+    the surplus is defined as the sales figures subtracted from the stock:
+    -positive surplus indicates waste
+    -negative surplus indicates extra made when stock was sold out
+    """
+    print("calculating surplus data... \n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
+    print(stock_row)
+
+
+def main():
+    """
+    Run all program functions
+    """
+    data = get_sales_data()
+    sales_data = [int(num) for num in data]
+    update_sales_worksheet(sales_data)
+    calculate_surplus_data(sales_data)
+
+
+print("Welcome to love Sandwiches Data Automation")
+main()
